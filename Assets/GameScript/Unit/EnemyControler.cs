14337189b5b12c5a9b316b470e35b3dbbfb88984
irispon <47470214+irispon@users.noreply.github.com>
2020-07-11 03:26:07 +0900

    [Script] Monster PathFind 제작중

---
+++ Assets/GameScript/Unit/EnemyControler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    SpriteRenderer sprite;
+    [SerializeField]
+    List<Transform> paths;
+    Queue<Transform> destinations;
+    [SerializeField]
+    Enemy enemy;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        enemy.speed = 1.5f;
+        destinations = new Queue<Transform>(paths);
+    }
+
+    public IEnumerator MoveTo(Transform transform)
+    {
+        Vector3 localPositon = this.transform.localPosition;
+        Vector3 targetPositon = transform.localPosition;
+        Debug.Log("움직임 " + localPositon +"  "+ targetPositon);
+      
+        for (; ((localPositon.x != targetPositon.x)&&(localPositon.y!= targetPositon.y));)
+        {
+            Debug.Log("움직임 "+ localPositon);
+            Vector3.MoveTowards(localPositon, targetPositon, enemy.speed);
+            yield return null;
+        }
+
+
+        if (destinations.Count > 0)
+        {
+           StartCoroutine( MoveTo(destinations.Dequeue()));
+        }
+
+    }
+    void Start()
+    {
+        if (destinations.Count > 0)
+        {
+            StartCoroutine(MoveTo(destinations.Dequeue()));
+        }
+   
+    }
+
+
+}
