faf1433c9ed9f19404418f445945dda0d67524da
irispon <47470214+irispon@users.noreply.github.com>
2020-07-18 12:44:11 +0900

    [Script,Resource]모든 탄환 애니메이션 적용, 애니메이션 이벤트 카피어 에디터 추가

---
+++ Assets/Utill/Editor/AnimationEventCopier.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class AnimationEventCopier : EditorWindow
+{
+    private AnimationClip sourceObject;
+    private AnimationClip targetObject;
+    [MenuItem("Window/Animation Event Copier")]
+    static void Init()
+    {
+        GetWindow(typeof(AnimationEventCopier));
+    }
+    void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        sourceObject = EditorGUILayout.ObjectField("Source", sourceObject, typeof(AnimationClip), true) as AnimationClip;
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.BeginHorizontal();
+        targetObject = EditorGUILayout.ObjectField("Target", targetObject, typeof(AnimationClip), true) as AnimationClip;
+        EditorGUILayout.EndHorizontal();
+
+        if (sourceObject != null && targetObject != null)
+        {
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Copy"))
+                CopyData();
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+
+    void CopyData()
+    {
+        Undo.RegisterCompleteObjectUndo(targetObject, "Undo Generic Copy");
+
+        AnimationClip sourceAnimClip = sourceObject as AnimationClip;
+        AnimationClip targetAnimClip = targetObject as AnimationClip;
+
+        if (sourceAnimClip && targetAnimClip)
+        {
+            AnimationUtility.SetAnimationEvents(targetAnimClip, AnimationUtility.GetAnimationEvents(targetAnimClip));
+            AnimationUtility.SetAnimationEvents(targetAnimClip, AnimationUtility.GetAnimationEvents(sourceAnimClip));
+        }
+    }
+}
